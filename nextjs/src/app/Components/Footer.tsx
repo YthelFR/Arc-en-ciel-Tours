695704180958ddc9b1408b8c5820f434870a7e51
Yoan <yoanjambon21@gmail.com>
2024-05-14 09:29:29 +0200

    MaJ symfony OK

---
+++ nextjs/src/app/Components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className="w-full bg-indigo-600 h-32">
+            <p>Footer</p>
+        </div>
+    )
+}
---
+++ nextjs/src/app/Components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="w-full bg-indigo-600 h-32">
+            <p>Header</p>
+        </div>
+    )
+}
